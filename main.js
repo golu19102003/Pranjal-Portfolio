da7b42cd5889e338200ec67d8235beafd85194f1
Pranjal Khandelwal <pranjalgurdawasi@gmail.com>
2025-05-29 21:31:44 +0530

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Data Science Enthusiast", "Salesforce Administrator","Software Developer", "Full Stack Web Developer","Salesforce Architect" ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
